18ff09d3b38f25dbbaf4725c482a058010df7f25
ryohamag <Ryojune21@gmail.com>
2025-03-11 15:44:50 +0900

    15:48

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_json():
         return jsonify({"error": "File not found"}), 404
 
 # スケジュール設定
-schedule.every().tuesday.at("15:43").do(job)
+schedule.every().tuesday.at("15:48").do(job)
 schedule.every().tuesday.at("16:00").do(get_race_json)
 
 def run_scheduler():
